068d81611b97083f264ad15b0ac8701601336503
chanble <chanbo.cn@gamil.com>
2015-01-25 21:27:28 +0800

    优化了代码

--- jquery.customselect.js
+++ jquery.customselect.js
@@ -6,19 +6,24 @@
 		var that = this;
 		var defaultOpts = {
 			//data中的value和text分别对应dataValue和dataText的值
-			'data' : [{'value': '1', 'text' : '第一个'},
-				{'value': '2', 'text' : '第二个第二个第二个第二个'},
-				{'value': '3', 'text' : '第三个'}],//string类型的值会被认为是地址
-			'dataValue' : 'value',
-			'dataText' : 'text',
-			'submitName' : 'bxdcustomselect',
-			'elementId' : 'bxdcustomselect',
-			'defaultText' : '请选择',
+			'data' : [
+				{'value': '0', 'html' : '请选择'},
+				{'value': '1', 'html' : '第一个'},
+				{'value': '2', 'html' : '第二个第二个第二个第二个'},
+				{'value': '3', 'html' : '第三个'},
+				{'value': '4', 'html' : '第四个'},
+				{'value': '5', 'html' : '第五个'},
+				{'value': '6', 'html' : '第六个'}
+			],//string类型的值会被认为是地址
+			'value' : 'value',
+			'html' : 'html',
+			'name' : 'cbcustomselect',//submit name
+			'id' : 'cbcustomselect',//element id
 			'onchange' : function(){
-				//console.log(this);
+				// console.log('change');
 			},
 			'ondone': function(){
-				
+				// console.log('done');
 			}
 		};
 		this.options = $.extend(false,defaultOpts, opts);
@@ -27,17 +32,17 @@
 		init();
 
 		function init(){
-			var hiddenInput = csinput = $('<input type="hidden" value="0"/>');
-			var hsubmitName = trim(that.options.submitName);
-			var helementId = trim(that.options.elementId);
+			var hiddenInput = csinput = $('<input type="hidden"/>');
+			var hsubmitName = trim(that.options['name']);
+			var helementId = trim(that.options['id']);
 			if(hsubmitName.length > 0){
 				hiddenInput.attr('name', hsubmitName);
 			}
 			if(helementId.length > 0){
 				hiddenInput.attr('id', helementId);
 			}
-			csspan = $('<span>'+ that.options.defaultText + '</span>');
-			that.el.addClass('bxd_customselect_span')
+			csspan = $('<span></span>');
+			that.el.addClass('cb_customselect_span')
 				.append(hiddenInput)
 				.append(csspan);
 			var sOptions = initSelectOptions(function(d){
@@ -47,7 +52,7 @@
 			});
 			optionlist.find('li').click(function(){
 				var oldCsInputValue = csinput.val();
-				setValue($(this).html(), $(this).attr(that.options.dataValue));
+				setValue($(this).html(), $(this).attr(that.options['value']));
 				if(oldCsInputValue != csinput.val()){
 					setTimeout(function(){
 						that.options.onchange.call(that);
@@ -66,7 +71,7 @@
 			}).mouseleave(function(){
 				optionlist.hide(200);
 			});
-			that.options.ondone.call(that);
+			select(0).options.ondone.call(that);
 		}
 		function trim(str){
 			if(!str){
@@ -94,28 +99,37 @@
 			if(typeof(d) == 'object' && d instanceof Array){
 				result = '<ul style="position: absolute; z-index: 999; display: none;">';
 				for(var i = 0; i < d.length; i++){
-					result += '<li value="'+ d[i][that.options.dataValue] + '" title="'
-						 + d[i][that.options.dataText] + '">' + d[i][that.options.dataText] + '</li>';
+					result += '<li value="'+ d[i][that.options['value']] + '" title="'
+						 + d[i][that.options.html] + '">' + d[i][that.options.html] + '</li>';
 				}
 			}
 			return result;
 		}
-		
+
 		//设置下拉框的值
 		function setValue(t, v){
 			csspan.html(t);
 			csinput.val(v);
+			return that;
 		}
-		
+
+		function select(index){
+			var selectedLi = $(optionlist).children(':eq('+index+')');
+			return setValue(selectedLi.html(), selectedLi.attr(that.options.value));
+		}
+
 		this.val = function(){
 			if(arguments.length < 1){
 				return csinput.val();
 			}else{
 				var v = arguments[0];
-				var selectedLi = optionlist.children("li["+ that.options.dataValue + "='"+ v +"']");
-				setValue(selectedLi.html(), selectedLi.attr(that.options.dataValue));
-				return that;
+				var selectedLi = optionlist.children("li["+ that.options.value + "='"+ v +"']");
+				return setValue(selectedLi.html(), selectedLi.attr(that.options.value));
 			}
+		}
+		//根据索引选择
+		this.select = function(index){
+			return select(index);
 		}
 	}
 })(jQuery);
